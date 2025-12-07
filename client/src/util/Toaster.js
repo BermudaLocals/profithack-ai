6fda5280063bc1e30f4ff0ab915cebd3cc8b223d
Bermuda Locals <31463815+BermudaLocals@users.noreply.github.com>
2025-12-07 22:48:09 +0000

    Delete client/src directory

--- client/src/util/Toaster.js
+++
@@ -1,9 +0,0 @@
-import { Position, Toaster, Intent } from "@blueprintjs/core";
- 
-/** Singleton toaster instance. Create separate instances for different options. */
-const AppToaster = Toaster.create({
-    className: "lm-toaster",
-    position: Position.TOP_RIGHT
-});
-
-export  default AppToaster;
